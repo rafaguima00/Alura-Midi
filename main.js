728202316ef23f6a4759e6a8f0d59405edf3d446
Rafael Moreira <rafael16gsm@gmail.com>
2023-03-05 14:15:37 -0300

    Novo commit

---
+++ main.js
@@ -0,0 +1,22 @@
+//Função com parâmetro é chamada quando clicamos no botão para tocar som
+function tocaSom(idElementAudio) {
+    document.querySelector(idElementAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (var i = 0; i < listaDeTeclas.length; i++) {
+
+    //Lista de teclas (botões) na nossa imagem
+    const tecla = listaDeTeclas[i];
+
+    //A segunda classe da lista de classes de cada botão
+    const instrumento = tecla.classList[1];
+
+    //Concatenar o id do som de cada botão com o nome das classes correspondentes
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+}
